04516346dae29bfd695a2e97b76bdc9a55180216
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-14 11:29:20 +0200

    colors container

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-fetch("https://www.thecolorapi.com/scheme?hex=07375b")
+let colors = [];
+
+function render() {
+  document.getElementById("colors-container").innerHTML = colors
+    .map((color) => {
+      return `
+    <div class="colors-palet">
+      <div class="color" style="background-color:${color.hex.value}";></div>
+      <div class="hex">${color.hex.value}</div>
+    </div>`;
+    })
+    .join("");
+}
+
+fetch(
+  "https://www.thecolorapi.com/scheme?hex=07375b&mode=monochrome&monochrome-dark&monochrome-light&analogic&complement&analogic-complement&triad&quad"
+)
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+    document.getElementById("mode-color").innerHTML = data.mode;
+    colors = data.colors;
+    render();
   });
-
-document.getElementById("get-color").addEventListener("click", () => {
-  console.log("color");
-});
