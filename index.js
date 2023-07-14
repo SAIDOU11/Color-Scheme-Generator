bbe8bb9734863f6191dc0875fce2e083d23479cc
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-14 06:43:18 +0200

    color

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-fetch("https://www.thecolorapi.com/id")
+fetch("https://www.thecolorapi.com/scheme?hex=07375b")
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
   });
+
+document.getElementById("get-color").addEventListener("click", () => {
+  console.log("color");
+});
