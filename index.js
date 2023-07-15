d1f3682f0cf20362430fbd12500b5b08f1faeac8
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-15 11:11:52 +0200

    Scheme color project

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-const listContainer = document.getElementById("list-color");
 let colors = [];
 let newString = "";
-// let monochrome = "monochrome";
-// let monochromeDark = "monochrome-dark";
+let mode = "";
 
-listContainer.addEventListener("click", (e) => {
-  console.log(e.target.id);
-});
-
-// &monochrome-dark&monochrome-light&analogic&complement&analogic-complement&triad&quad
+const dropdown = document.querySelectorAll(".dropdown");
 const input = document.getElementById("pick-color");
 
 let url = `https://www.thecolorapi.com/scheme?hex=07375b&mode=monochrome`;
@@ -17,9 +11,7 @@ function getColor() {
   document.getElementById("get-color").addEventListener("mouseup", () => {
     let inputValue = input.value;
     newString = inputValue.substring(1);
-    console.log(input.value, inputValue, newString);
-    url = `https://www.thecolorapi.com/scheme?hex=${newString}&mode=${monochrome}`;
-    console.log(url);
+    url = `https://www.thecolorapi.com/scheme?hex=${newString}&mode=${mode}`;
     let req = new Request(url);
     sendRequest(req);
   });
@@ -46,11 +38,6 @@ function getColor() {
     .join("");
 }
 
-document.getElementById("dropdown").addEventListener("click", () => {
-  const list = document.querySelector(".list-color");
-  list.style.display = "block";
-});
-
 function render() {
   document.getElementById("colors-container").innerHTML = colors
     .map((color) => {
@@ -63,12 +50,43 @@ function render() {
     .join("");
 }
 
+dropdown.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelectorAll(".menu li");
+  const selected = dropdown.querySelector(".selected");
+
+  // Add a click to select element
+  select.addEventListener("click", () => {
+    // Add click to select element
+    select.classList.toggle("select-clicked");
+    menu.classList.toggle("menu-open");
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      mode = option.innerText;
+      selected.innerText = option.innerText;
+      if ((selected.innerText = mode)) {
+        url = `https://www.thecolorapi.com/scheme?hex=07375b&mode=${mode}`;
+        req = new Request(url);
+      }
+      select.classList.remove("select-clicked");
+      menu.classList.remove("menu-open");
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+      // Add active class
+      option.classList.add("active");
+    });
+  });
+});
+
 fetch(url, {
   method: "GET",
 })
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
     document.getElementById("mode-color").innerHTML = data.mode;
     colors = data.colors;
     render();
